8fa96fea7544734957f8f0c75cc1233be4392e34
jayaraman <106896556+jayaramanbio@users.noreply.github.com>
2022-10-09 14:27:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,179 @@
+ //import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container-fluid bg-dark">
+            <div className="container">
+                <div className="text-center text-white">
+                    DINESH.
+                </div>
+            </div>
+        </div> 
+        <div className="container-fluid">
+            <div className="container">
+                <div className="row">
+                    <div className="col-6">
+                        <img src="./template2img1.jpg" width="88%" alt=""/>
+                    </div>
+                    <div className="col-6 ">
+                        <div className="row mb-5">
+                            <h1 className="">Hi guys!</h1>
+                        <p className="fs-5 ">
+                            I have been working as a Web  developer sinnce sept,2000 till date and  as a Freelancer since Jul,2004.That's about 20 Years as of now.
+                            I love nature.I always prefer to be  aligned with the nature .wanna be wanderer forever!Life is at  peace  when  your next thing is somewhere,out of  nowhere.
+                            acquied about a bit of knowledge spanning for about 8 years now.still on the hunt!
+                            I'm a Frenlance Technical Trainer,training students,employees,wanna be  aspirant
+                            Developers  from all sectors in technologies like HTML,CSS,Javascipt,Bootstrap,PHP with occasional mongoDB and MySQL.
+                            Want and will groom the next generation a better person in termsof technical  or  person aspect!
+                        </p>
+                        <div className="col-4 list-group">
+                            <ul className="name">
+                                <p><b>Name</b></p>
+                                <p><b>Experience</b></p>
+                                <p><b>Country</b></p>
+                                <p><b>Location</b></p>
+                                <p><b>e-mail</b></p>
+                                <p><b>Phone</b></p>
+                            </ul>
+                            <a href="/#"  role="button" class="btn btn-primary">
+                                <i class="fa-thin fa-folder-arrow-down"></i>
+                            DOWNLOAD RESUME</a>
+                            
+                        </div>
+                        <div className="col-4">
+                            
+                                <p>Dinesh</p>
+                                <p>20 Years</p>
+                                <p>India</p>
+                                <p>Chennai</p>
+                                <p>srinanoo@gmail.com</p>
+                                <p>+1234567890</p>
+                                <a href="/#" role="button" class="btn btn-danger mt-3 text-white"><i class="fa-thin fa-message-lines"></i>CONTACT ME</a>
+                        </div>
+                        <div className="col-4 "></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="container-fluid bg-secondary">
+            <div className="container">
+                <h1 className="text-center py-3">What I do</h1>
+                <div className="row py-3">
+                    <div className="col-3 bg-white mx-2 text-center">
+                        <h3>WEB DEVELOPMENT</h3>
+                        <p>WordPress,PHP,MySQL,HTML,CSS,
+                            JS,Bootstrap,Opencart,XML,ASP,
+                            SQL server,
+                            Design Conversions,clickFunnels,etc.,
+                        </p>
+                    </div>
+                    <div className="col-3 bg-white mx-2 text-center">
+                        <h3>TECHNICAL TRAINER</h3>
+                        <p>HTML,CSS,Bootstrap,Javascipt,
+                            PHP,MySQL,mongoDP</p>
+                    </div>
+                    <div className="col-3 bg-white mx-2 text-center">
+                        <h3>ARCHERY</h3>
+                        <p >Archer Practitioner,Archery Level 1 
+                            state coach,Archery state
+                              Judge</p>
+                    </div>
+                    <div className="col-2 bg-white mx-2 text-center">
+                        <h3>FITNESS EXPERT</h3>
+                        <p>Researching on fitness related to 
+                            Archery for 6 years.will keep
+                            Researching on fitness related to 
+                            Archery.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="container-fluid">
+            <div className="container">
+                <div className="row">
+                    <div className="col-6">
+                        <h3>EDUCATION</h3>
+                    <div className="bg-secondary">
+                        <p>Msc(IT)</p>
+                        <p>year<br/>Description goes here...</p>
+                    </div>
+                    <div className="bg-secondary">
+                        <p>Msc(IT)</p>
+                        <p>year<br/>Description goes here...</p>
+                    </div>
+                    <div class="bg-secondary">
+                        <p>Msc(IT)</p>
+                        <p>year<br/>Description goes here...</p>
+                    </div>
+                    </div>
+                    <div className="col-6">
+                        <h3>EXPERIENCE</h3>
+                    <div className="bg-secondary">
+                    <p>Msc(IT)</p>
+                    <p>year<br/>Description goes here...</p>
+                    </div>
+                    <div className="bg-secondary">
+                        <p>Msc(IT)</p>
+                        <p>year<br/>Description goes here...</p>
+                    </div>
+                    <div className="bg-secondary">
+                        <p>Msc(IT)</p>
+                        <p>year<br/>Description goes here...</p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="container-fluid bg-secondary">
+            <div className="container">
+                <h2 className="text-center py-5">Works</h2>
+                <div className="row">
+                    <div className="col-4">
+                        <img src="./template2img2.jpg"  width="95%" alt=""/>
+                    </div>
+                    <div className="col-4">
+                        <img src="./template2img3.jpg" width="95%" alt=""/>
+                    </div>
+                    <div className="col-4">
+                        <img src="./template2img4.jpg" width="95%" alt=""/>
+                    </div>
+                </div>
+                <div className="row py-4">
+                    <div className="col-4">
+                        <img src="./template2img5.jpg"  width="95%" alt=""/>
+                    </div>
+                    <div className="col-4">
+                        <img src="./template2img6.jpg"  width="95%" alt=""/>
+                    </div>
+                    <div className="col-4">
+                        <img src="./template2img7.jpg" width="95%" alt=""/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="container-fluid">
+            <div className="container">
+                <div className="row text-center p-3 ">
+                    <div className="col-4"></div>
+                    <div className="col-1"><i className="fa-brands fa-facebook"></i></div>
+                    <div className="col-1"><i className="fa-brands fa-twitter"></i></div>
+                    <div className="col-1"><i className="fa-brands fa-instagram"></i></div>
+                    <div className="col-1"><i className="fa-brands fa-linkedin"></i></div>
+                    <div className="col-1"><i className="fa-brands fa-google"></i></div>
+                    <div className="col-3"></div>
+                </div>
+                <div  className="text-center pb-4">
+                    Copyright<i className="fa-regular fa-copyright"></i> 2021.All Right Reserved
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+  );
+}
+
+export default App;
